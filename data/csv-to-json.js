5c166c21dc1f5e76ec54ab7e3224714aca93e154
Marian Rusnak <grizly21@gmail.com>
2016-11-02 22:50:04 +0000

    Refactoring of data converter

--- data/csv-to-json.js
+++ data/csv-to-json.js
@@ -1,25 +1,41 @@
-var fs = require('fs');
-var csv = require('csv');
+const fs = require('fs');
+const csv = require('csv');
 
-fs.readFile('workshops.csv', (err, data) => {
-    csv.parse(data, (err, data) => {
-        const csvEntries = data.slice(1);
-        const workshops = csvEntries.map((csvEntry) => {
-            return {
-                name: csvEntry[3],
-                description: csvEntry[4],
-                timeFrame: csvEntry[8],
-                regions: csvEntry[5].split(', '),
-                lecturer: {
-                    name: csvEntry[1],
-                    role: csvEntry[2]
-                }
-            };
-        });
-        // console.log(workshops);
+const pathToCsv = process.argv[2] || `${__dirname}/workshops.csv`;
+const pathToJson = `${__dirname}/workshops.json`;
 
-        fs.writeFile('./filter/workshops.json', JSON.stringify(workshops, null, '  '), () => {
-            console.log(`${workshops.length} workshops converted from CSV to JSON.`);
-        });
+console.log(`Converting '${pathToCsv}'`);
+
+fs.readFile(pathToCsv, (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  csv.parse(data, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const csvEntries = data.slice(1);
+    const workshops = csvEntries.map(csvEntry => {
+      return {
+        name: csvEntry[3],
+        description: csvEntry[4],
+        timeFrame: csvEntry[8],
+        regions: csvEntry[5].split(', '),
+        lecturer: {
+          name: csvEntry[1],
+          role: csvEntry[2]
+        }
+      };
     });
+
+    console.log(`Writing to '${pathToJson}'`);
+
+    fs.writeFile(pathToJson, JSON.stringify(workshops, null, '  '), () => {
+      console.log(`${workshops.length} workshops converted from CSV to JSON.`);
+    });
+  });
 });
